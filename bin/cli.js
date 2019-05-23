f2e15e4732909844d14e9884605bf9cd7ab612e3
noyobo <noyobo@gmail.com>
2019-05-23 20:28:08 +0800

    feat: add command help tips

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,40 @@
 #!/usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2));
+const parseArgs = require('minimist');
+
+const argv = parseArgs(process.argv.slice(2), {
+  alias: {
+    s: 'skip-lint',
+    o: 'output',
+    v: 'version',
+    h: 'help',
+  },
+});
 
 const prettier = require('../lib/prettier');
 const { _: files, ...options } = argv;
 
+if (argv.help) {
+  help();
+}
+if (argv.version) {
+  console.log(require('../package.json').version);
+  process.exit(0);
+}
 prettier(files, options);
+
+function help() {
+  console.log(`
+Usage: prettier-style [options] <files>
+
+Options:
+  -v, --version          Output the version number
+  -o, --output           Formated files dest folder path
+  -k, --skip-lint        Skip stylelint
+  -h, --help             Output usage information
+
+Example:
+  prettier-style -s ./src/**/*.css
+`);
+  process.exit(0);
+}
